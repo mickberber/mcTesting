e077affd2d06d8d33250ac6e9b47411d0a730946
Michael Berber <mickberber@icloud.com>
2016-05-14 18:39:54 -0700

    add compare objects

--- tests.js
+++ tests.js
@@ -1,6 +1,13 @@
-function tests() {};
+var tests = {};
 
-tests.isString(value) {
+tests.typeof = function(value) {
+  if(Array.isArray(value)) {
+    return 'array';
+  }
+  return typeof value;
+};
+
+tests.isString = function(value) {
   if(typeof value === 'string') {
     return true;
   } else {
@@ -8,7 +15,7 @@ tests.isString(value) {
   }
 };
 
-tests.isArray(value) {
+tests.isArray = function(value) {
   if(Array.isArray(value)) {
     return true;
   } else {
@@ -16,7 +23,7 @@ tests.isArray(value) {
   }
 };
 
-tests.isObject(value) {
+tests.isObject = function(value) {
   if(typeof value === 'object') {
     return true;
   } else {
@@ -24,35 +31,38 @@ tests.isObject(value) {
   }
 };
 
-tests.isNumber(value) {
+tests.isNumber = function(value) {
+  if(value.toString() === 'NaN') {
+    return 'NaN';
+  }
   if(typeof value === 'number') {
     return true;
   } else {
     return false;
   }
-}
+};
 
-test.isFunction(value) {
+tests.isFunction = function(value) {
   if(typeof value === 'function') {
     return true;
   } else {
     return false;
   }
-}
+};
 
-tests.length(value) {
+tests.length = function(value) {
   if(value.length) {
     return value.length;
   } else {
     return 'No length property available.';
   }
-}
+};
 
-tests.compareArrays() {
+tests.compareArrays = function() {
   var args = Array.prototype.slice.call(arguments);
   for(var i = 0; i < args[0].length; i++) {
     for(var j = 1; j < args.length; j++) {
-      if(test.length(args[j]) !== test.length(args[0])) {
+      if(tests.length(args[j]) !== tests.length(args[0])) {
         return false;
       }
       if(args[0][i] !== args[j][i]) {
@@ -61,6 +71,37 @@ tests.compareArrays() {
     }
   }
   return true;
-}
+};
+
+tests.compareObjects = function() {
+  var args = Array.prototype.slice.call(arguments);
+  for(var key in args[0]) {
+    var type = tests.typeof(args[0][key]);
+    if(type === 'array') {
+      for(var i = 1; i < args.length; i++) {
+        if(!tests.compareArrays(args[0][key], args[i][key])){
+          return false;
+        }
+      }
+    }
+    if(type === 'object') {
+      for(var i = 1; i < args.length; i++) {
+        if(!tests.compareObjects(args[0][key], args[i][key])){
+          return false;
+        }
+      }
+    }
+    for(var i = 1; i < args.length; i++) {
+      if(type === 'NaN' && args[i][key].toString() !== 'NaN') {
+        return false;
+      }
+      if(args[0][key] !== args[i][key]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
 
 module.exports = tests;
