8702d7faaf7ce97df74b837f62e8ad9a385b62db
Michael Berber <mickberber@icloud.com>
2016-05-18 03:10:27 -0700

    test isNumber

---
+++ testTheTests/isNumber.js
@@ -0,0 +1,44 @@
+var tests = require('./../tests.js');
+var write = tests.write;
+
+function callback() {
+  return [];
+}
+
+function callbackObj() {
+  return {};
+}
+
+function callbackStr() {
+  return 'string';
+}
+
+write.testFile(
+  write.description('isNumber should return true when passed a string')
+  (tests.compareYield(
+    tests.isNumber('string'), false)),
+  write.description('isNumber should return false when passed an array')
+    (tests.compareYield(
+      tests.isNumber([]), false)),
+  write.description('isNumber should return false when passed an object')
+    (tests.compareYield(
+      tests.isNumber({}), false)),
+  write.description('isNumber should return true when passed NaN')
+    (tests.compareYield(
+      tests.isNumber(NaN), true)),
+  write.description('isNumber should return true when passed a number')
+    (tests.compareYield(
+      tests.isNumber(22), true)),
+  write.description('isNumber should return false when passed a function')
+    (tests.compareYield(
+      tests.isNumber(function(){}), false)),
+  write.description('isNumber should return false when passed a callback that returns a string')
+    (tests.compareYield(
+      tests.isNumber(callback()), false)),
+  write.description('isNumber should return false when passed a callback that returns an array')
+    (tests.compareYield(
+      tests.isNumber(callback()), false)),
+  write.description('isNumber should return false when a function returns an object')
+    (tests.compareYield(
+      tests.isNumber(callbackObj()), false))
+);
