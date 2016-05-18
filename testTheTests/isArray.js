08492bdbe35029d66f97021c8bd9ef80d0ddb534
Michael Berber <mickberber@icloud.com>
2016-05-18 02:04:09 -0700

    finish testing isArray

--- testTheTests/isArray.js
+++ testTheTests/isArray.js
@@ -1,15 +1,36 @@
 var tests = require('./../tests.js');
 var write = tests.write;
 
+function callback() {
+  return [];
+};
+
+function callbackObj() {
+  return {};
+}
+
 write.testFile(
   write.description('isArray should return true when passed an array')
     (tests.isArray([])),
   write.description('isArray should return false when passed a string')
-    (tests.compareYield(tests.isArray('string'), false)),
+    (tests.compareYield(
+      tests.isArray('string'), false)),
   write.description('isArray should return false when passed an object')
-    (tests.compareYield(tests.isArray({}), false)),
+    (tests.compareYield(
+      tests.isArray({}), false)),
   write.description('isArray should return false when passed NaN')
-    (tests.compareYield(tests.isArray(NaN), false)),
+    (tests.compareYield(
+      tests.isArray(NaN), false)),
   write.description('isArray should return false when passed a number')
-    (tests.compareYield(tests.isArray(22), false))
+    (tests.compareYield(
+      tests.isArray(22), false)),
+  write.description('isArray should return false when passed a function')
+    (tests.compareYield(
+      tests.isArray(function(){}), false)),
+  write.description('isArray should return true when passed a callback that returns an array')
+    (tests.compareYield(
+      tests.isArray(callback()), true)),
+  write.description('isArray should return false when a function returns an object')
+    (tests.compareYield(
+      tests.isArray(callbackObj()), false))
 );
