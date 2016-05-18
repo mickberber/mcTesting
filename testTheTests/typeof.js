a8650156054434ed1a8275df74690c33fb8edb55
Michael Berber <mickberber@icloud.com>
2016-05-18 02:23:17 -0700

    test typeof

---
+++ testTheTests/typeof.js
@@ -0,0 +1,23 @@
+var tests = require('./../tests.js');
+var write = tests.write;
+
+write.testFile(
+  write.description('typeof should return \'array\' when passed an array')
+    (tests.compareYield(
+      tests.typeof([]), 'array')),
+  write.description('typeof should return \'string\' when passed a string')
+    (tests.compareYield(
+      tests.typeof('string'), 'string')),
+  write.description('typeof should return \'object\' when passed a object')
+    (tests.compareYield(
+      tests.typeof({}), 'object')),
+  write.description('typeof should return \'NaN\' when passed a NaN')
+    (tests.compareYield(
+      tests.typeof(NaN), 'NaN')),
+  write.description('typeof should return \'number\' when passed a number')
+    (tests.compareYield(
+      tests.typeof(22), 'number')),
+  write.description('typeof should return \'boolean\' when passed a boolean')
+    (tests.compareYield(
+      tests.typeof(3 > 2), 'boolean'))
+);
