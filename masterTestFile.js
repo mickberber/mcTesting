708eee6d1eca49bf146118856191c4fa949768f5
Michael Berber <mickberber@icloud.com>
2016-05-24 00:57:36 -0700

    MTF naming

--- masterTestFile.js
+++ masterTestFile.js
@@ -1,14 +1,14 @@
 var tests = require('./mcTesting.js');
 var write = tests.write;
-var arrayify = require('./testTheTests/arrayify.js');
-var isArrayTests = require('./testTheTests/isArray.js');
-var isNumberTests = require('./testTheTests/isNumber.js');
-var isObjectTests = require('./testTheTests/isObject.js');
-var isStringTests = require('./testTheTests/isString.js');
-var typeOfTests = require('./testTheTests/isFunction.js');
-var length = require('./testTheTests/length.js');
-var primitives = require('./testTheTests/primitives.js');
-var compArrays = require('./testTheTests/compareArrays.js');
+var arrayify = require('./tests/arrayify.js');
+var isArrayTests = require('./tests/isArray.js');
+var isNumberTests = require('./tests/isNumber.js');
+var isObjectTests = require('./tests/isObject.js');
+var isStringTests = require('./tests/isString.js');
+var typeOfTests = require('./tests/isFunction.js');
+var length = require('./tests/length.js');
+var primitives = require('./tests/primitives.js');
+var compArrays = require('./tests/compareArrays.js');
 
 tests = [
   arrayify,
