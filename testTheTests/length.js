9c954b09096eca2b8c284331e1d65cedb1d035a1
Michael Berber <mickberber@icloud.com>
2016-05-23 23:50:07 -0700

    continue testing compare primitives and length

--- testTheTests/length.js
+++ testTheTests/length.js
@@ -4,8 +4,14 @@ var write = tests.write;
 module.exports = write.testFile(
   write.description('tests.length should return the length of an array')(
     tests.compareYield(
-      tests.length([1,2,3,4]), 4)),
+      tests.length([1,2,3,4]),
+      4)),
   write.description('tests.length should return the length of a string')(
     tests.compareYield(
-      tests.length('string'), 6))
+      tests.length('string'),
+      6)),
+  write.description('tests.length should return false when passed a value with no length')(
+    tests.compareYield(
+      tests.length({}),
+      false))
 );
--- testTheTests/primitives.js
+++ testTheTests/primitives.js
@@ -2,10 +2,24 @@ var tests = require('./../tests.js');
 var write = tests.write;
 
 module.exports = write.testFile(
-  write.description('compare primitives should return a boolean')(
-    tests.compareYield(
-      tests.comparePrimitives('string', 'string'), true)),
   write.description('compare primitives should return true when passed two strings')(
     tests.compareYield(
-      tests.comparePrimitives('string', 'string'), true))
+      tests.comparePrimitives('string', 'string'),
+      true)),
+  write.description('compare primitives should return false when passed a number and a string')(
+    tests.compareYield(
+      tests.comparePrimitives(42, 'string'),
+      false)),
+  write.description('compare primitives should return false when passed a string and object')(
+    tests.compareYield(
+      tests.comparePrimitives('string', {}),
+      false)),
+  write.description('compare primitives should return false when passed a number and object')(
+    tests.compareYield(
+      tests.comparePrimitives(22, {}),
+      false)),
+  write.description('compare primitives should return false when passed a boolean and object')(
+    tests.compareYield(
+      tests.comparePrimitives(true, {}),
+      false))
 );
